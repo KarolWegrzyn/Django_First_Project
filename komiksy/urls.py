2b413341426b401a44c40b731d51156af01c476f
Węgrzyn_Karol <rwegrzynkarol@gmail.com>
2024-04-20 22:28:02 +0200

    comit1

--- komiksy/urls.py
+++ komiksy/urls.py
@@ -3,12 +3,16 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib.auth import views
+from django.urls import path
+from komiksyweb.views import login_view, logout_view
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('komiksy/', include('komiksyweb.urls')), #dodanie wszystkich url z pliku komiksyweb -> urls
-    path('login/', views.LoginView.as_view(), name="login"),
+    #path('login/', views.LoginView.as_view(), name="login"),
+    path('login/', login_view, name='login_url'),
+    path('logout/', logout_view, name='logout_url'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 
--- komiksyweb/views.py
+++ komiksyweb/views.py
@@ -5,6 +5,29 @@ from .forms import KomiksForm
 from django.contrib.auth import logout
 from django.conf import settings
 from django.shortcuts import redirect
+from django.contrib.auth import authenticate, login, logout
+from django.shortcuts import render, redirect
+
+
+def login_view(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            # Przekierowanie na stronę po zalogowaniu.
+            return redirect('wszystkie_komiksy')
+        else:
+            # Komunikat o błędnych danych logowania.
+            return render(request, 'login.html', {'error_message': 'Invalid login'})
+    else:
+        return render(request, 'login.html')
+
+def logout_view(request):
+    logout(request)
+    # Przekierowanie na stronę po wylogowaniu.
+    return redirect('login_url')
 
 def wszystkie_komiksy(request): #dodanie metody zwracajacej widok (zwraca wszystkie rekordy z bazy)
     wszystkie = Komiks.objects.all() #pobranie wszystkich obiektow z bazy (ORM)
